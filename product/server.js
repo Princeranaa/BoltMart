68fbf0a5fd1e900763bb25fab22d058398e4f775
Prince Rana <ranaprince45may@gmail.com>
2025-12-02 20:11:57 +0530

    modify the port number

--- product/server.js
+++ product/server.js
@@ -3,7 +3,7 @@ const app = require("./src/app");
 const db = require("./src/config/databse");
 db.connectDB();
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT
 
 app.listen(PORT, ()=>{
     console.log(`Product service is running on port 3001`);
